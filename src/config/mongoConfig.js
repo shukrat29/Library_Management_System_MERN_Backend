ed792804cf51276feb9675bfcdf916d389694f85
shukra <shukratamang.npl@gmail.com>
2024-05-17 11:04:55 +1000

    Backend set up

---
+++ src/config/mongoConfig.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export const connectMongoDB = async () => {
+  try {
+    const con = await mongoose.connect(process.env.MONGO_URL);
+    con && console.log('MongoDB connected');
+  } catch (error) {
+    console.log(error);
+  }
+};
